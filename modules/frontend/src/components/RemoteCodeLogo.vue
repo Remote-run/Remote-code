908c8f93f513995adc6e1a14f835d2154d2e2c13
trygve <trygve.woldseth@gmail.com>
2021-07-15 17:30:15 +0200

    made logo and stuff

---
+++ modules/frontend/src/components/RemoteCodeLogo.vue
@@ -0,0 +1,85 @@
+<template lang="html">
+  <div class="remote-code-logo"
+       :style="{gridRowGap: fontSize * 0.03 + 'em', gridColumnGap: fontSize * 0.02 + 'em', fontSize: fontSize +'em'}">
+
+    <h1 id="top">Remote</h1>
+    <h1 id="bottom">Code</h1>
+    <div id="spacer" :style="{height: spikeHeight }"/>
+    <div id="longspike" :style="abc"/>
+    <div id="shortspike" :style="abc"/>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+export default class RemoteCodeLogo extends Vue {
+  @Prop({ default: 10 }) fontSize!: number;
+  @Prop({ default: '10px' }) spikeHeight!: string;
+  // @Prop({ default: 20 }) colGap!: number;
+  // @Prop({ default: 20 }) rowGap!: number;
+
+  abc = {
+    // width: this.fontSize * 0.0055 + 'em'
+    width: 0.09 + 'em'
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+$mainBlue: #00008f;
+$secondaryBlue: #0001db;
+
+.remote-code-logo {
+  display: grid;
+  width: fit-content;
+
+  #longspike {
+    background-color: $secondaryBlue;
+    //width: 20px;
+    //height: 30px;
+    grid-column: 3;
+    grid-row: 1/3;
+  }
+
+  #shortspike {
+    background-color: $secondaryBlue;
+    //width: 20px;
+    //height: 30px;
+    grid-column: 2;
+    grid-row: 1/4;
+  }
+
+  #spacer {
+    grid-column: 1;
+    grid-row: 1/2;
+  }
+
+  h1 {
+    color: #707070;
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    font-size: inherit;
+    text-align: right;
+
+    line-height: 0.75;
+    //height: 30px;
+
+    //margin-bottom: auto;
+
+    #top {
+      grid-row: 2;
+    }
+
+    #bottom {
+
+      grid-row: 3;
+    }
+  }
+
+}
+
+</style>
--- modules/frontend/src/views/Home.vue
+++ modules/frontend/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <RemoteCodeLogo font-size="5" spike-height="200px"></RemoteCodeLogo>
+    <!--    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { Provide } from 'vue-property-decorator'
+import RemoteCodeLogo from '@/components/RemoteCodeLogo.vue' // @ is an alias to /src
 
 @Options({
   components: {
+    RemoteCodeLogo,
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Provide('abc') lalalala = 'shitty language'
+}
+
 </script>
