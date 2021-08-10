94f603c3ff8073ffa010f3de4ee83d24cc43b20b
trygve <trygve.woldseth@gmail.com>
2021-08-10 12:25:17 +0200

    fixed rights and slave startup

--- start_slave.sh
+++ start_slave.sh
@@ -6,6 +6,10 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 set -e
 trap popd EXIT
 
+# Build the base container
+# TODO: should be automated later
+modules/slave/container_envs/_base/build.sh
+
 # start the slave
 pushd ./modules/master/deploy
 docker-compose up -d --build
